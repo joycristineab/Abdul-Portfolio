329d4aefaeddd7a548f9b564b1b14cdbbe9c66dc
joycristineab <abdul.cristinejoyy@gmail.com>
2025-09-21 10:50:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+      strings: ['Tvl-Ict student','Artist','Animator', 'Frontend Developer', 'Web Designer'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1200,
+      loop: true,
+
+    });
+
